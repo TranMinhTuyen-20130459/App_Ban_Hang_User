fd79bebd79f63710ea0ebe438958bfd9076f4230
Lenh Nguyen <20130303@st.hcmuaf.edu.vn>
2023-11-25 08:44:42 +0700

    update style

--- styles.js
+++ styles.js
@@ -2,7 +2,6 @@ import { StyleSheet } from "react-native";
 
 export const globalStyles = StyleSheet.create({
   text: {
-    fontFamily:
-      'BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+    fontFamily: '"Segoe UI", Roboto, Helvetica, Arial, sans-serif',
   },
 });
